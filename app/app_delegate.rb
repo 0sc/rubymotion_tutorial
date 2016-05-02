12ecfbf91604244ab5a23f399f97746fb83d3f0c
Osmond Oscar <osmond.oranagwa@andela.com>
2016-05-02 11:29:06 +0100

    A little more about controllers

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,28 +1,19 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
 
-    rootViewController = UIViewController.alloc.init
-    rootViewController.title = 'HelloMotion'
-    rootViewController.view.backgroundColor = UIColor.whiteColor
+    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+    other_controller = UINavigationController.alloc.initWithRootViewController(controller)
+    other_controller.title = "Other"
+    other_controller.view.backgroundColor = UIColor.purpleColor
 
-    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller.viewControllers = [nav_controller, other_controller]
 
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = navigationController
+    # @window.rootViewController = navigationController
+    @window.rootViewController = tab_controller
     @window.makeKeyAndVisible
-
-
-    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 100, 100, 100))
-    @blue_view.backgroundColor = UIColor.blueColor
-    @window.addSubview(@blue_view)
-
-    @green_view = UIView.alloc.initWithFrame(CGRectMake(10, 100, 50, 50))
-    @green_view.backgroundColor = UIColor.greenColor
-    @window.addSubview(@green_view)
-
-    @red_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 50, 50))
-    @red_view.backgroundColor = UIColor.redColor
-    @blue_view.addSubview(@red_view)
     true
   end
 end
