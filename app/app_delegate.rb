6973d35c89ebdb8f868cfc7a9c8ffda92e2ab325
Osmond Oscar <osmond.oranagwa@andela.com>
2016-05-03 18:47:31 +0100

    Basic Animation

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -16,6 +16,26 @@ class AppDelegate
     # @window.rootViewController = navigationController
     @window.rootViewController = tab_controller
     @window.makeKeyAndVisible
+
+    @points = [[0, 0], [50, 0], [0, 50], [50, 50]]
+    @current_index = 0
+
+    @view = UIView.alloc.initWithFrame [@points[@current_index], [100, 100]]
+    @view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@view)
+
+    animate_to_next_point
+
     true
+  end
+
+  def animate_to_next_point
+    @current_index += 1
+
+    @current_index = @current_index % @points.count
+
+    UIView.animateWithDuration(2, delay: 1, options: UIViewAnimationOptionCurveLinear, animations: lambda {
+      @view.frame = [@points[@current_index], [100, 100]]
+      }, completion: lambda{ |finished| self.animate_to_next_point })
   end
 end
